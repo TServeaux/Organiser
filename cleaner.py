47ace2d277582aa7d6baf5a2dd6dcb602755d079
TServeaux <taoserveauxx@gmail.com>
2026-02-03 22:23:55 +0100

    Creation of fonction

---
+++ cleaner.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import json
+
+def normaliePrices() :
+    return
+
+def standardizeDates() :
+    return
+
+def removeDuplicates() :
+    return
+
+def mergesFiles():
+    return
+
+def cleanColumns():
+    #sert à uniformiser le nom des colonnes
+    return
+
+def mergeFiles():
+    return
